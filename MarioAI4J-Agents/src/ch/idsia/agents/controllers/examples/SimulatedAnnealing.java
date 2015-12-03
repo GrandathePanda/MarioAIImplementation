973373bfb729973278a8232685c245a6c7ff7e5a
Nicolai Ferraris <Nicolai@Nicolais-MBP.home>
2015-12-03 14:02:28 -0500

    Simulated Annealing

--- MarioAI4J-Agents/src/ch/idsia/agents/controllers/examples/SimulatedAnnealing.java
+++ MarioAI4J-Agents/src/ch/idsia/agents/controllers/examples/SimulatedAnnealing.java
@@ -1,7 +1,10 @@
-//Better than BFS, still not great.
-
 package ch.idsia.agents.controllers.examples;
 
+import java.awt.Graphics;
+import java.util.*;
+
+import EnemyActorPhysics.MyMario;
+import EnemyActorPhysics.MySprite;
 import ch.idsia.agents.AgentOptions;
 import ch.idsia.agents.IAgent;
 import ch.idsia.agents.controllers.MarioHijackAIBase;
@@ -10,22 +13,23 @@ import ch.idsia.benchmark.mario.engine.LevelScene;
 import ch.idsia.benchmark.mario.engine.VisualizationComponent;
 import ch.idsia.benchmark.mario.engine.generalization.Enemy;
 import ch.idsia.benchmark.mario.engine.input.MarioInput;
+import ch.idsia.benchmark.mario.engine.input.MarioKey;
 import ch.idsia.benchmark.mario.environments.IEnvironment;
 import ch.idsia.benchmark.mario.options.FastOpts;
+import togepi.GraphGenerator;
+import togepi.genPair;
+import togepi.GraphGenerator.*;
+import togepi.Pair;
 
-import java.awt.*;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Vector;
+import static togepi.GraphGenerator.mapCopy;
 
-/**
- * Your custom agent! Feel free to fool around!
- * 
- * @author Jakub 'Jimmy' Gemrot, gemrot@gamedev.cuni.cz
- */
 public class SimulatedAnnealing extends MarioHijackAIBase implements IAgent {
-
+	private final Action[] allPossibleActions = {Action.Jump,Action.LeftLongJump,Action.RightLongJump,Action.Left,Action.Right,Action.LeftSpeed,Action.RightSpeed};
+	private final Action[] airPossibleActions = {Action.LeftLongJump,Action.RightLongJump,Action.Left,Action.Right};
+	private final Action[] groundPossibleActions = {Action.Jump,Action.Left,Action.Right,Action.LeftSpeed,Action.RightSpeed};
+	private final Action[] airHangPossibleActions = {Action.Left,Action.Right};
+	boolean g1 = false; //deal with air drop in the beggining
+	private boolean shooting = false;
 	
 	@Override
 	public void reset(AgentOptions options) {
@@ -37,171 +41,202 @@ public class SimulatedAnnealing extends MarioHijackAIBase implements IAgent {
 		super.debugDraw(vis, level, env, g);
 		// provide custom visualization using 'g'
 	}
+	/**
+	 * 
+	 * @param x First element X coordinate
+	 * @param y First element Y coordinate
+	 * @param otherX Second element X coordinate
+	 * @param otherY Second element Y coordinate
+	 * @return Haversine distance between the two.
+	 */
 
+	private MarioInput doActions(Vector<Action> ac) {
+		MarioInput doThese = new MarioInput();
+		for(Action x : ac) {
+//			System.out.println(x);
+			switch (x) {
+				case Jump:
+					doThese.set(MarioKey.JUMP, mario.mayJump);
+					break;
+				case RightLongJump:
+					doThese.press(MarioKey.JUMP);
+					doThese.release(MarioKey.LEFT);
+					doThese.press(MarioKey.RIGHT);
+				case LeftLongJump:
+					doThese.press(MarioKey.JUMP);
+					doThese.release(MarioKey.RIGHT);
+					action.press(MarioKey.LEFT);
+				case Right:
+					doThese.release(MarioKey.LEFT);
+					doThese.press(MarioKey.RIGHT);
+					break;
+				case RightSpeed:
+					doThese.release(MarioKey.LEFT);
+					doThese.press(MarioKey.RIGHT);
+					doThese.press(MarioKey.SPEED);
+					break;
+				case Left:
+					doThese.release(MarioKey.RIGHT);
+					doThese.press(MarioKey.LEFT);
+					break;
+				case LeftSpeed:
+					doThese.release(MarioKey.RIGHT);
+					doThese.press(MarioKey.LEFT);
+					doThese.press(MarioKey.SPEED);
+					break;
+			}
+		}
+		return doThese;
+	}
+	
+	public double distance(int x, int y, int otherX, int otherY){
+		int curr = (otherX - x ) * (otherX - x) + (otherY - y)  * (otherY - y);
+		return Math.sqrt(curr);
+	}
+	
+
+	public genPair<Pair, genPair<Action, HashMap<Pair,Node>>> pickRandom(Vector<genPair<Pair,genPair<Action,HashMap<Pair,Node>>>> vec){
+		int index = (int)Math.random()/vec.size();
+		return vec.get(index);
+	}
+
+	public genPair<Pair, genPair<Action, HashMap<Pair,Node>>> pickBetter(genPair<Pair, genPair<Action, HashMap<Pair,Node>>> p1,genPair<Pair, genPair<Action, HashMap<Pair,Node>>> p2 ){
+		int heuristic1 = 0;
+		int heuristic2 = 0;
+		if(p1.x != p2.x){
+			System.out.println("err");
+		}
+		Pair marioLoc = p1.x;
+		genPair<Action,HashMap<Pair,Node>> p1Child = p1.y;
+		
+		if(p1Child.y.get(marioLoc).enemyHere){
+			--heuristic1;
+		}
+		if(p1Child.y.get(marioLoc).blockHere){
+			--heuristic1;
+		}
+		if(p1Child.y.get(marioLoc).xPos > 0){
+			++heuristic1;
+		}
+		genPair<Action,HashMap<Pair,Node>> p2Child = p2.y;
+		if(p2Child.y.get(marioLoc).enemyHere){
+			--heuristic2;
+		}
+		if(p2Child.y.get(marioLoc).blockHere){
+			--heuristic2;
+		}
+		if(p2Child.y.get(marioLoc).xPos > 0){
+			++heuristic2;
+		}		
+		genPair<Pair, genPair<Action, HashMap<Pair,Node>>> ret = (heuristic1 > heuristic2) ?  p1 :  p2;
+		return ret;
+	}
 
 	
-	//Node class, Shouldn't have to touch this. 
-	private class Node {
-		public boolean enemyHere = false;
-		public boolean blockHere = false;
-		public boolean goal = false;
-		public boolean seen = false; //prevents cycles in the searching algorithms
-		public boolean frontier = false; // also prevents cycles if for some reason the first fails.
-		public int xPos = 0;
-		public int yPos = 0;
-		public Vector<Node> children = new Vector<Node>();
-		public void reset() { // update the node to the current values
-			seen = false; 
-			frontier = false;
-			blockHere = t.brick(xPos,yPos);
-			enemyHere = e.danger(xPos,yPos);
-		}
-		public Node(int x, int y) {
-			xPos = x;
-			yPos = y;
-			blockHere = t.brick(x,y);
-			enemyHere = e.danger(x,y);
-			
-		}
-	}
-	
-	//Pair class to use in the Graphs node indexing. You shouldn't have to touch this.
-	private class Pair {
-		public int x = 0;
-		public int y = 0;
-		
-		@Override
-		public int hashCode() { // Again don't have to touch this but cool thing worth noting.
-			// A pair of integers can form a bijection(one to one and onto, thus unique and useful for hashing) to a single integer ZxZ->Z
-			// This is a modified cantor pairing function, that maps positive and negative integers into
-			// a computationally less expensive set, contained in 32 bits for unsigned and 64 bit for signed.
-			int A = x >= 0 ? 2 * x : -2 * x - 1;
-			int B = y >= 0 ? 2 * y : -2 * y - 1;
-			int code = A >= B ? A * A + A + B : A + B * B;
-			return code;
-		}
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj)
-				return true;
-			if (obj == null)
-				return false;
-			if (getClass() != obj.getClass())
-				return false;
-			Pair other = (Pair)obj;
-			if(this.x == other.x && this.y == other.y)
-				return true;
-			return false;
-		}
-		public Pair(int a, int b) {
-			x = a;
-			y = b;
-		}
-		
-		
-	}
-	public HashMap<Pair,Node> generateGraph() {
-		int gridSize = 4; //Controls how big the simulated graph is. Feel free to tweak.
-		HashMap<Pair,Node> Graph = new HashMap<Pair,Node>();
-		//Generate all Nodes in the selected size range.
-		for( int i = 0; i <= gridSize; i++ ) {
-			for ( int j = 0; j>= -gridSize; j--) {
-				Node currentNode = new Node(i,j);
-				Graph.put(new Pair(i,j),currentNode);
-			}
-		}
-		/*Create a list from the graph of all nodes in the graph in no particular order.
-		 * Iterate through the list. At any given node all we have to do is test for the boundaries.
-		 * If a node is a boundary point, don't generate children outside of the grid. If the node
-		 * is not a boundary generate all children nodes in the up down forward and back direction.
-		 * This forms the edges of the graph, every node has at most four children. 
-		 * This finishes creating our undirected graph.
-		 */
-		Collection<Node> listNodes = Graph.values();
-		Iterator<Node> listIter = listNodes.iterator();
-		while(listIter.hasNext()) {
-			Node iterable = listIter.next();
-			if((iterable.yPos-1<-gridSize) == false) {
-				Node childUp = Graph.get(new Pair(iterable.xPos,iterable.yPos-1)); //Example of using a pair to check the HashMap
-				iterable.children.add(childUp);
-			}
-			if((iterable.yPos+1>0) == false) {
-				Node childDown = Graph.get(new Pair(iterable.xPos,iterable.yPos+1));
-				iterable.children.add(childDown);
-			}
-			if(((iterable.xPos+1)>gridSize) == false) {
-				Node childForward = Graph.get(new Pair(iterable.xPos+1,iterable.yPos));
-				iterable.children.add(childForward);
-			}
-			else { iterable.goal = true;}
-			if((iterable.xPos-1<0) == false) {
-				Node childBackward = Graph.get(new Pair(iterable.xPos-1,iterable.yPos));
-				iterable.children.add(childBackward);
-			}	
-		}
-
-		
-		return Graph;
-	}
-	
-	
-	public int costFunction(Vector<Node> possibleSol){
-		int currentCost = 0;
-		for(Node each: possibleSol){
-			if(each.blockHere){
-				++currentCost;
-			}
-			if(each.enemyHere){
-				currentCost += 2;
-			}
-			if(each.goal){
-				currentCost -= 1000;
-			}
-		}
-		
-		return currentCost;
-	}
-	
-		//Make the graph a class level variable so it keeps its state. Only have to generate once.
-		//Keep from here---------
-		public HashMap<Pair,Node> Graph = null;
-		public Collection<Node> List = null;
-		public boolean isGraphGenerated = false;
-	public MarioInput actionSelectionAI() {
-		if( isGraphGenerated == false ) { //If the graph hasn't been generated yet, generate it.
-			Graph = generateGraph();
-			List = Graph.values();
-			isGraphGenerated = true;
+	boolean graph = false;
+	GraphGenerator Graph = null;
+	Action[] possibleActions = {Action.Jump,Action.LeftLongJump,Action.RightLongJump,Action.Left,Action.Right,Action.LeftSpeed,Action.RightSpeed};
+	public MarioInput actionSelectionAI(){
+		Vector<HashMap<Pair,Node>> solutionStates = new Vector<HashMap<Pair, Node>>();
+		LinkedList<HashMap<Pair,Node>> frontierStates = new LinkedList<HashMap<Pair,Node>>();
+		Vector<HashMap<Pair,Node>> seenStates = new Vector<HashMap<Pair,Node>>();
+		Vector<Action> bestInTime = new Vector<>();
+		if(!graph) { //If the graph hasn't been generated yet, generate it.
+			Graph = new GraphGenerator(9,9,mario);
+			Graph.generateGraph(e,t);
+			Graph.isGraphGenerated = true;
+			graph = true;
 		}
 		else {
-			Iterator<Node> resetNodes = List.iterator(); //If it has been generated just update all the nodes. 
-			while(resetNodes.hasNext()) {
-				Node resetNode = resetNodes.next();
-				resetNode.reset();
-			}
-		}
-		Node StartNode = Graph.get(new Pair(0,0));
-		//--------- To here for what ever you want to do
-		Node current = StartNode;
-		
-		
-		
-		
-		
-		//Your code Here 
-		
+			Graph.resetNodes(e,t);
 
-		System.gc(); //Suggest garbage clean-up after work here.
-		return action;
+		}
+		MyMario simM = Graph.State.get(new Pair(0,0)).alterMario;
+		Action[] modPosAction;
+        if(simM.onGround) {
+                modPosAction = groundPossibleActions;
+        }
+        else {
+                if(!simM.onGround)
+                        modPosAction = airPossibleActions;
+                else modPosAction = airHangPossibleActions;
+        }
+
+		int runs = 0;
+		boolean firstStateSeenCurrent = false;
+		frontierStates.push(Graph.State);
+		solutionStates.add(Graph.State);
+		Vector<Action> toDo = new Vector<Action>();
+		while(!frontierStates.isEmpty() && runs < 9){
+			HashMap<Pair,Node> currentState = frontierStates.removeFirst();
+			solutionStates.add(currentState);
+			seenStates.add(currentState);
+			Vector<genPair<Pair,genPair<Action,HashMap<Pair,Node>>>> childStates = new Vector<genPair<Pair,genPair<Action,HashMap<Pair,Node>>>>();
+			if (firstStateSeenCurrent) {
+				System.out.println("HERE TickModel");
+				childStates = Graph.tickModel(currentState, modPosAction);
+			} else {
+				System.out.println("HERE Tick");
+				childStates = Graph.tick(currentState, modPosAction);
+				firstStateSeenCurrent = true;
+			}
+//			System.out.println(childStates.size());
+			genPair<Pair,genPair<Action,HashMap<Pair,Node>>> option1 = pickRandom(childStates);
+			childStates.remove(option1);
+			genPair<Pair,genPair<Action,HashMap<Pair,Node>>> option2 = pickRandom(childStates);
+			genPair<Pair,genPair<Action,HashMap<Pair,Node>>> act = pickBetter(option1, option2);
+			System.out.println(act.y.x);
+			toDo.add(act.y.x);
+
+			++runs;
+		}
+		
+		return doActions(toDo);
 	}
+	/*
+	public MarioInput actionSelectionAI2(){
+		if(!graph){
+			Graph = new GraphGenerator(9,9,mario);
+			Graph.generateGraph(e, t);
+			Graph.isGraphGenerated = true;
+			graph = true;
+		}
+		else {
+			Graph.resetNodes(e, t);
+		}
+		int runs = 0;
+		Vector<HashMap<Pair,Node>> states = new Vector<HashMap<Pair, GraphGenerator.Node>>();
+		states.add(mapCopy(Graph.State));
+		HashMap<Pair,Node> curr = states.get(0);
+		Pair p2 = new Pair(9,9);
+		Node p = curr.get(p2);
+		Vector<genPair<Pair,genPair<Action,HashMap<Pair,Node>>>> possibleMoves = null;
+		possibleMoves = Graph.tickModel(curr, possibleActions);
+
+		possibleMoves.get(0).y.y.get(p2);
+		
+		Node marioNode = null;
+		Vector<genPair<Pair, MySprite>> existingEntities = new Vector<>();
+		for(Map.Entry<Pair, Node> et: curr.entrySet()){
+			Node cNode = et.getValue();
+			Pair pos = new Pair(cNode.xPos, cNode.yPos);
+			List<MySprite> cNodeEntities = cNode.modelEntitiesHere;
+			for(MySprite x: cNodeEntities){
+				existingEntities.add(new genPair<>(pos,x));
+			}
+			if(cNode.mario) marioNode = cNode;
+		}
+		
+		genPair<Node, Action> myAct = pickBest(possibleMoves, marioNode);
+		Action toDo = myAct.y;
 	
-	public static void main(String[] args) {
-		String options = FastOpts.FAST_VISx2_02_JUMPING + FastOpts.L_ENEMY(Enemy.GOOMBA);
-		
+		System.gc();
+		return action;
+	}*/
+	public static void main(String[] args){
+		String options = FastOpts.FAST_VISx2_02_JUMPING+FastOpts.L_DIFFICULTY(0)+FastOpts.L_ENEMY(Enemy.GOOMBA);
 		MarioSimulator simulator = new MarioSimulator(options);
-		
 		IAgent agent = new SimulatedAnnealing();
-		
 		simulator.run(agent);
 		
 		System.exit(0);
